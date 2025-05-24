0e432aa2f6efb7adee128407af11cc9072ad9f6a
Mr.DuoReMi <zxhas@foxmail.com>
2025-05-24 14:52:37 +0800

    [improve] Add filesystem to provide path relative functions.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,7 +2,14 @@
 
 #include <QApplication>
 
+#include "Utils/CSJPathTool.h"
+
 int main(int argc, char *argv[]) {
+
+    /* Record the work directory. */
+    CSJPathTool *pathTool = CSJPathTool::getInstance();
+    pathTool->setWorkDirectory(fs::canonical(fs::path(argv[0]).remove_filename()));
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
